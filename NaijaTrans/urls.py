6df82000d7bc2fcb06e6d00116bac0f602928f2e
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2024-10-11 03:31:45 +0000

    Automated commit

--- NaijaTrans/urls.py
+++ NaijaTrans/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include  # Include allows linking to app-level urls.py
 
+from .models import UserLogin  # or your custom user model name
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('userlogin.urls')),  # Include the app-level URLs for userlogin app
-    path('api/', include('userregister.urls')),  # Include the app-level URLs for userregister app
+    path('auth/', include('UserLogin.urls')),  # Include the app-level URLs for userlogin app
+    #path('api/', include('userregister.urls')),  # Include the app-level URLs for userregister app
 ]
 
